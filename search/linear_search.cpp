b3eb0646d96713edb27690aa4072fc1d6837d5f7
Muhammadrasul <muhammadrasul446@mail.ru>
2020-05-18 04:25:41 +0500

    Linear search

---
+++ search/linear_search.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+bool linear_search(int arr[], int n, int x) {
+	int i;
+	for (i = 0; i < n; i++) {
+		if (arr[i] == x) {
+			return true;
+		}
+	}
+	return false;
+}
+
+int main(int argc, char const *argv[])
+{
+	int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	int n = sizeof(arr) / sizeof(arr[0]);
+	cout << linear_search(arr, n, 7) << endl;
+	return 0;
+}
